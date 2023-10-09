a3ce046fd2d3a97e82d139c1f500837c52349c85
__ysfk <21014438+YusufKocyigit@users.noreply.github.com>
2023-10-09 16:28:12 +0200

    v2 dev first commit

--- content.js
+++ content.js
@@ -1,6 +1,15 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.font) {
-      document.body.style.fontFamily = message.font;
-    }
-  });
-  
+// This script runs on web pages to change the font if enabled
+console.log("Content script is running.");
+
+chrome.storage.sync.get(['applyFont', 'selectedFont'], function(result) {
+  if (result.applyFont) {
+    const selectedFont = result.selectedFont || 'arial';
+    console.log('Selected font:', selectedFont);
+
+    // Check if this log message appears in the console
+    console.log('Applying font:', selectedFont);
+
+    // Apply the selected font here
+    document.body.style.fontFamily = selectedFont === 'arial' ? 'Arial, sans-serif' : 'Roboto, sans-serif';
+  }
+});
--- popup.js
+++ popup.js
@@ -1,7 +1,31 @@
-document.getElementById('applyFont').addEventListener('click', function () {
-    const selectedFont = document.getElementById('fontSelector').value;
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { font: selectedFont });
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  // Retrieve and update the state of font and whitelist checkboxes
+  chrome.storage.sync.get(['applyFont', 'applyToWhitelisted', 'selectedFont'], function(result) {
+    document.getElementById('fontCheckbox').checked = result.applyFont || false;
+    document.getElementById('whitelistCheckbox').checked = result.applyToWhitelisted || false;
+    
+    // Set the selected font based on the stored value
+    const selectedFont = result.selectedFont || 'arial';
+    document.getElementById('fontSelect').value = selectedFont;
   });
-  
+
+  // Attach event listeners for checkbox changes
+  document.getElementById('fontCheckbox').addEventListener('change', function() {
+    const applyFont = this.checked;
+    chrome.storage.sync.set({ applyFont });
+  });
+
+  document.getElementById('whitelistCheckbox').addEventListener('change', function() {
+    const applyToWhitelisted = this.checked;
+    chrome.storage.sync.set({ applyToWhitelisted });
+  });
+
+  // Handle font selection change
+  document.getElementById('fontSelect').addEventListener('change', function() {
+    const selectedFont = this.value;
+    chrome.storage.sync.set({ selectedFont });
+    // You may want to communicate with content.js to apply the selected font immediately
+  });
+
+  // ... rest of the code for other buttons
+});
